64bd1682211398e28d6582123fb28e58ca878913
NaduniRanasinghe <naduni.k.ranasinghe@gmail.com>
2019-06-02 23:41:29 +0530

    DBHandler

--- DBHandler.java
+++ DBHandler.java
@@ -3,6 +3,7 @@ package com.example.naduniranasinghe.androidappmodelpaoer;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DBHandler extends SQLiteOpenHelper {
 
@@ -12,12 +13,49 @@ public class DBHandler extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String createQuery = "CREATE TABLE "+UserProfile.Users.TABLE_NAME+"";
+        String createQuery = "CREATE TABLE "+UserProfile.Users.TABLE_NAME+"("+UserProfile.Users.COL_ID+
+                "INTEGER PRIMARY KEY AUTOINCREMENT ,"+UserProfile.Users.COL_USERNAME+"TEXT UNIQUE,"+
+                UserProfile.Users.COL_PASSWORD+"TEXT,"+UserProfile.Users.COL_GENDER+"TEXT"+UserProfile.Users.COL_DOB+"TEXT"+")";
+
+        Log.d("createQuery",createQuery);
+
+        try{
+            db.execSQL(createQuery);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+        }
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String createQuery = "CREATE TABLE "+UserProfile.Users.TABLE_NAME+"("+UserProfile.Users.COL_ID+
+                "INTEGER PRIMARY KEY AUTOINCREMENT ,"+UserProfile.Users.COL_USERNAME+"TEXT UNIQUE,"+
+                UserProfile.Users.COL_PASSWORD+"TEXT,"+UserProfile.Users.COL_GENDER+"TEXT"+UserProfile.Users.COL_DOB+"TEXT"+")";
+
+        Log.d("createQuery",createQuery);
+
+        try{
+            db.execSQL(createQuery);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Log.e("Exception",e.getMessage());
+        }
 
     }
-}
+
+    public boolean addInfo(UserProfile.Users users){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        String insertQuery = "INSERT INTO" + UserProfile.Users.TABLE_NAME;
+        
+return true;
+
+    }
+
+    }
+
